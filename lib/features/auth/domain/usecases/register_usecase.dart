a7218788230fedb97b4193f0f7a5df707d229e9e
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2024-10-22 18:29:18 +0300

    Refactor RegisterUsecase to use email and password parameters instead of UserEntity

--- lib/features/auth/domain/usecases/register_usecase.dart
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -1,13 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:social_app/core/errors/failure.dart';
-import 'package:social_app/features/auth/domain/entities/user_entity.dart';
+import 'package:social_app/features/auth/domain/repositories/auth_repostitory.dart';
 
 class RegisterUsecase {
-  final AuthRepository repository;
+  final AuthRepostitory repository;
 
   RegisterUsecase(this.repository);
 
-  Future<Either<Failure, Unit>> call(UserEntity params) async {
-    return await repository.register(params);
+  Future<Either<Failure, Unit>> call(String email, String password) async {
+    return await repository.register(email, password);
   }
 }
