9ddc0174c3e9224780c2be6b545b0142ba06a4d5
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2024-10-25 16:48:24 +0300

    Refactor AuthScreen widget and remove unused code

--- lib/features/auth/presentation/presentation_layer.dart
+++ lib/features/auth/presentation/presentation_layer.dart
@@ -1,4 +1,3 @@
 export 'package:social_app/features/auth/presentation/cubit/auth_cubit.dart';
-export 'package:social_app/features/auth/presentation/pages/auth_screen.dart';
 export 'package:social_app/features/auth/presentation/pages/register_screen.dart';
 export 'package:social_app/features/auth/presentation/pages/Login_screen.dart';
