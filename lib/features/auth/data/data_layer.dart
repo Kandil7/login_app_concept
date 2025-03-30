11a1c36f46778dcd2565dddad0d19c0f2f6b31da
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2025-03-30 15:45:40 +0200

    Add initial TypeScript and lodash files to the project

--- lib/features/auth/data/data_layer.dart
+++
@@ -1,5 +0,0 @@
-export 'models/user_model.dart';
-export 'models/login_model.dart';
-export 'models/register_model.dart';
-export 'repositories/auth_repostitory_imp.dart';
-export 'datasources/remote_datasource/auth_remote_datasource.dart';
--- lib/features/auth/presentation/presentation_layer.dart
+++
@@ -1,3 +0,0 @@
-export 'package:social_app/features/auth/presentation/cubit/auth_cubit.dart';
-export 'package:social_app/features/auth/presentation/pages/register_screen.dart';
-export 'package:social_app/features/auth/presentation/pages/Login_screen.dart';
